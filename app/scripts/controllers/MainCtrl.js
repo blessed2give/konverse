f6cf84ad0514a03cc78ceef83d89ad29ba1d5816
Paul <paucho1437@yahoo.com>
2017-07-24 16:42:33 -0500

    weather forecasted

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -2,14 +2,20 @@
     function MainCtrl($scope, $http) {
       // Initialize global variables
       var apiKey = '79c4eb4b276398cb69787312bf3bbd08';
+      var totalTemp = 0;
 
       $scope.send = function() {
         // set variables to access api
-        var city = 'Chicago';
-        var openWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&mode=json&units=imperial&cnt=16&appid=' +apiKey;
+        var city = $scope.city;
+        var openWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&mode=json&units=imperial&cnt=' + $scope.forecast + '&appid=' +apiKey;
 
         $http.get(openWeatherUrl)
+        // function to calculate, then retrieve average temp based on forecast option
           .then(function (response) {
+            for (var i = 0; i < $scope.forecast; i++) {
+              totalTemp += response.data.list[i].temp.day;
+            }
+            $scope.avgTemp = totalTemp / $scope.forecast;
           }, function(response) {
             $scope.currentWeather = "Something went wrong";
           });
